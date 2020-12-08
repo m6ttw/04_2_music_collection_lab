6b3219d0d5eab3253b4cf6090cb920da2e061dae
m6ttw <matthewrhyswilliams@live.com>
2020-12-08 16:53:39 +0000

    databases working

--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -1,16 +1,16 @@
 from db.run_sql import run_sql
 
 from models.artist import Artist
-# from models.album import Album
+from models.album import Album
 
 
-def save(user):
+def save(artist):
     sql =  "INSERT INTO artists (name) VALUES (%s) RETURNING *"
     values = [artist.name]
     results = run_sql(sql, values)
     id = results[0]['id']
-    user.id = id
-    return user
+    artist.id = id
+    return artist
 
 
 def select_all():
@@ -47,7 +47,7 @@ def delete(id):
     run_sql(sql, values)
 
 
-def update(user):
+def update(artist):
     sql = "UPDATE artists SET name = %s WHERE id = %s"
     values = [artist.name, artist.id]
     run_sql(sql, values)
